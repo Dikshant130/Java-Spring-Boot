a0d58e45b36be7b2fefdfb567dd503ac38238144
Dikshant Tyagi <dikshant.tyagi4131@gmail.com>
2024-03-06 16:06:38 +0530

    first commit

---
+++ quickstart/src/main/java/com/dikshant/quickstart/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.dikshant.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "/helloworld")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}
